b13fb8852e7cc52f5f900dcbf56197b060bc19db
Pedro Porta <pedrohmp@icloud.com>
2020-11-11 22:27:51 -0300

    add: hotel model, setting class

--- modules/Hotel/SettingClass.php
+++ modules/Hotel/SettingClass.php
@@ -4,18 +4,21 @@ namespace  Modules\Hotel;
 
 use Modules\Core\Abstracts\BaseSettingsClass;
 use Modules\Core\Models\Settings;
+use Custom\Hotel\CustomSettingsPage;
 
 class SettingClass extends BaseSettingsClass
 {
     public static function getSettingPages()
     {
+        $customSettings = new CustomSettingsPage();
+
         return [
             [
                 'id'   => 'hotel',
                 'title' => __("Hotel Settings"),
                 'position'=>20,
                 'view'=>"Hotel::admin.settings.hotel",
-                "keys"=>[
+                "keys"=>$customSettings->customSettings([
                     'hotel_disable',
                     'hotel_page_search_title',
                     'hotel_page_search_banner',
@@ -47,14 +50,7 @@ class SettingClass extends BaseSettingsClass
                     'hotel_deposit_type',
                     'hotel_deposit_amount',
                     'hotel_deposit_fomular',
-
-                    'hotel_contract',
-                    'hotel_contract_title',
-                    'hotel_inspection_term',
-                    'hotel_inspection_term_title',
-                    'hotel_internal_regime',
-                    'hotel_internal_regime_title',
-                ],
+                ]),
                 'html_keys'=>[
 
                 ]
