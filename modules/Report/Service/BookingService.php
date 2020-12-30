a072b604c229b2420b0e39dfb15bc6dc0c9ba732
tiagoyg <tiagoyg@gmail.com>
2020-12-30 19:52:32 -0300

    function mask

--- modules/Report/Service/BookingService.php
+++ modules/Report/Service/BookingService.php
@@ -12,13 +12,13 @@ class BookingService
 {
     function getContract($contract, $booking)
     {
-        $user       = User::query()->find($booking->customer_id)->first();
-        $service    = $booking->service;
+        $user = User::query()->find($booking->customer_id)->first();
+        $service = $booking->service;
 
-        $contract = str_replace("[name_client]", $user->first_name.' '.$user->last_name, $contract);
+        $contract = str_replace("[name_client]", $user->first_name . ' ' . $user->last_name, $contract);
         $contract = str_replace("[address_client]", $user->address, $contract);
         $contract = str_replace("[complement_address_client]", $user->address2, $contract);
-        $contract = str_replace("[zipcode_address_client]", $user->zip_code,$contract);
+        $contract = str_replace("[zipcode_address_client]", $this->mask("#####-###", $user->zip_code), $contract);
         $contract = str_replace("[phone_client]", $user->phone, $contract);
         $contract = str_replace("[phone_zap_client]", $user->phone2, $contract);
         $contract = str_replace("[cpf_client]", $user->cpf_cnpj, $contract);
@@ -31,11 +31,25 @@ class BookingService
         /*$contract = str_replace("[value_signal]", $booking->pay_now, $contract);
         $contract = str_replace("[value_signal]", $booking->pay_now, $contract);*/
 
-        $today = Carbon::now();
-        $currentDate = 'São Paulo, ' . $today->day . ' do ' . $today->month . ' de ' .  $today->year;
 
-$contract = str_replace("[date_current]", $currentDate, $contract);
+        $today = Carbon::now();
+        $currentDate = 'São Paulo, ' . $today->day . ' do ' . $today->month . ' de ' . $today->year;
+
+        $contract = str_replace("[date_current]", $currentDate, $contract);
 
         return $contract;
     }
+
+    function mask($mask, $str)
+    {
+        $str = str_replace(" ", "", $str);
+
+        for ($i = 0; $i < strlen($str); $i++) {
+            $mask[strpos($mask, "#")] = $str[$i];
+        }
+
+        return $mask;
+    }
 }
+
+
