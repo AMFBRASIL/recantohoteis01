d62f33e6c74ff935265e95909c72bdaca58977ec
tiagoyg <tiagoyg@gmail.com>
2021-03-25 13:44:00 -0300

    novos campos tipo de pensao

--- modules/Reservation/Models/PensionType.php
+++ modules/Reservation/Models/PensionType.php
@@ -14,5 +14,37 @@ class PensionType extends Model
 
     protected $fillable = [
         'name',
+        'daily_rate_40',
+        'daily_rate_100',
+        'start_time',
+        'end_date'
     ];
+
+    public function setDailyRate40Attribute($value)
+    {
+        $this->attributes['daily_rate_40'] = str_replace(',', '.', str_replace('.', '', $value));
+    }
+
+    public function getDailyRate40FormattedAttribute()
+    {
+        $value = '0,00';
+        if ($this->daily_rate_40) {
+            $value = number_format($this->daily_rate_40, 2, ',', '.');
+        }
+        return $value;
+    }
+
+    public function setDailyRate100Attribute($value)
+    {
+        $this->attributes['daily_rate_100'] = str_replace(',', '.', str_replace('.', '', $value));
+    }
+
+    public function getDailyRate100FormattedAttribute()
+    {
+        $value = '0,00';
+        if ($this->daily_rate_100) {
+            $value = number_format($this->daily_rate_100, 2, ',', '.');
+        }
+        return $value;
+    }
 }
