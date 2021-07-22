d880914ca49213b0199646ab566293311557dd54
Tiago Perrelli <tiago.perrelli@fulcrumdigital.com>
2021-07-22 20:19:13 -0300

    dashboard fixed

--- modules/Dashboard/Admin/DashboardController.php
+++ modules/Dashboard/Admin/DashboardController.php
@@ -15,26 +15,28 @@ class DashboardController extends AdminController
     public function index()
     {
         $f = strtotime('monday this week');
+
         $data = [
-            'recent_bookings'    => Booking::getRecentBookings(),
-            'top_cards'          => Booking::getTopCardsReport(),
-            'earning_chart_data' => Booking::getDashboardChartData($f, time())
+            'recent_bookings'               => Booking::getRecentBookings(8),
+            'restaurant_orders'             => $this->restaurantOrders(),
+            'top_cards'                     => Booking::getTopCardsReport(),
+            'earning_chart_data'            => Booking::getDashboardChartData($f, time()),
+            'earning_chart_payment_data'    => Booking::getDashboardChartPaymentData($f, time())
         ];
         return view('Dashboard::index', $data);
     }
 
     public function reloadChart(Request $request)
     {
-            $chart = $request->input('chart');
-            switch ($chart) {
-                case "earning":
-                    $from = $request->input('from');
-                    $to = $request->input('to');
-                    return $this->sendSuccess([
-                        'data' => Booking::getDashboardChartData(strtotime($from), strtotime($to))
-                    ]);
-                    break;
-            }
+        $from = $request->input('from');
+        $to = $request->input('to');
+
+        return $this->sendSuccess([
+            'data' => [
+                'earning_chart_data'            => Booking::getDashboardChartData($from, $to),
+                'earning_chart_payment_data'    => Booking::getDashboardChartPaymentData($from, $to)
+            ]
+        ]);
     }
 
     public function situations(Request $request)
